7d33b98caa64c44871a7cadf87e0800e5459c09b
co2y <co2y@foxmail.com>
2016-04-28 10:16:46 +0800

    add

---
+++ 120.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def minimumTotal(self, triangle):
+        """
+        :type triangle: List[List[int]]
+        :rtype: int
+        """
+        if not triangle:
+            return 0
+        
+        l = len(triangle)
+        ans = 0
+        
+        dp = triangle[-1][:]
+        
+        for i in range(l-2,-1,-1):
+            temp = []
+
+            for j in range(len(triangle[i])):
+                temp.append(min(dp[j]+triangle[i][j],dp[j+1]+triangle[i][j]))
+
+            dp = temp[:]
+        return dp[0]
+        
