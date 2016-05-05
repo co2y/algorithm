3db367df42a16c9334f7d57997f7df10918a30cc
co2y <co2y@foxmail.com>
2016-05-05 20:34:02 +0800

    Gas Station

--- 134.py
+++ 134.py
@@ -6,3 +6,7 @@ class Solution(object):
             if cumsum < minimum:
                 minimum, min_idx = cumsum, i
         return min_idx if cumsum >= 0 else -1
+
+本题关键在于利用题目给的条件：
+只有唯一的解，如果一圈的油量足够走完一定有解，
+并且cumsum最小的点才有可能是唯一的那个点。
