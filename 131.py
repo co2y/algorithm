203d2e796582d72da788e1dc448ad97180824303
co2y <co2y@foxmail.com>
2016-05-23 10:46:25 +0800

    DFS+DP: Palindrome Partitioning

--- 131.py
+++ 131.py
@@ -26,3 +26,38 @@ def partition(self, s):
             if isPal(s[j:i]):
                 dp[i]+=[each+[s[j:i]] for each in dp[j]]
     return dp[-1]
+    
+    
+class Solution(object):
+    def partition(self, s):
+        """
+        :type s: str
+        :rtype: List[List[str]]
+        """
+        
+        def isPalindrome(subs):
+            left = 0
+            right = len(subs)-1
+            
+            while left < right:
+                if subs[left] != subs[right]:
+                    return False
+                else:
+                    left += 1
+                    right -= 1
+            return True
+            
+        
+        def dfs(s, path):
+            if not s:
+                res.append(path)
+            for i in range(1,len(s)+1):
+                if isPalindrome(s[:i]):
+                    dfs(s[i:], path+[s[:i]])
+        
+        if not s:
+            return []
+        res = []   
+        dfs(s,[])
+        return res
+                    
