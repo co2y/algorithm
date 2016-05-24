7fbbb03e8f33b7ca65f3d9b3d9204d1fe608f270
co2y <co2y@foxmail.com>
2016-05-24 10:58:07 +0800

    Product of Array Except Self

--- 238.py
+++ 238.py
@@ -1,1 +1,17 @@
-左右乘
+class Solution(object):
+    def productExceptSelf(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        l = len(nums)
+        res = [1] * l
+        
+        for i in range(1, l):
+            res[i] = res[i-1] * nums[i-1]
+            
+        for i in range(l-1, 0, -1):
+            res[i] = res[i] * res[0]
+            res[0] = res[0] * nums[i]
+            
+        return res
