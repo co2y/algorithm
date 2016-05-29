6173ad2b030ebc33b6f055a25c3351b6a241d4e9
co2y <co2y@foxmail.com>
2016-05-29 17:16:38 +0800

    Reverse String

--- 344.py
+++ 344.py
@@ -7,3 +7,20 @@ class Solution(object):
         return s[::-1]
 
 java use StringBuilder not String
+python str 不能直接赋值，例如s[1] = '5'
+
+class Solution(object):
+    def reverseString(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        ss = list(s)
+        left = 0
+        right = len(s) - 1
+        while left < right:
+            ss[left],ss[right] = ss[right],ss[left]
+            left += 1
+            right -= 1
+        return "".join(ss)
+        
