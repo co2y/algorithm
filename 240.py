f0cb8b001a4b8bba2e95bffccc3bcc2ef749419f
co2y <co2y@foxmail.com>
2016-06-05 22:58:35 +0800

    beautiful O(n+m):Search a 2D Matrix II

---
+++ 240.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def searchMatrix(self, matrix, target):
+        """
+        :type matrix: List[List[int]]
+        :type target: int
+        :rtype: bool
+        """
+        if not matrix:
+            return False
+            
+        m = len(matrix)
+        n = len(matrix[0])
+        
+        i = 0
+        j = n-1
+        while i<m and j>=0:
+            if target > matrix[i][j]:
+                i += 1
+            elif target < matrix[i][j]:
+                j -= 1
+            else:
+                return True
+        
+        return False
