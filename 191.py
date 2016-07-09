b737863c31afa8ba40f05868933fe2be90c59ae1
co2y <co2y@foxmail.com>
2016-07-09 12:34:19 +0800

    Number of 1Bits

---
+++ 191.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def hammingWeight(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        count = 0
+        while n!=0:
+            count += 1
+            n = n & (n-1)
+            
+        return count
+    
+s = Solution()
+r = s.hammingWeight(-1)
+print r
