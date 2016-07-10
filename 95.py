6942faf6943ffd34c19aa232a1bed589d5c713b7
co2y <co2y@foxmail.com>
2016-07-10 11:55:04 +0800

    Unique Binary Search Trees II

---
+++ 95.py
@@ -0,0 +1,28 @@
+# https://discuss.leetcode.com/topic/15886/should-be-6-liner
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def generateTrees(self, n):
+        """
+        :type n: int
+        :rtype: List[TreeNode]
+        """
+        def node(val, left, right):
+            node = TreeNode(val)
+            node.left = left
+            node.right = right
+            return node
+            
+        def trees(first, last):
+            return [node(root, left, right)
+                    for root in range(first, last+1)
+                    for left in trees(first, root-1)
+                    for right in trees(root+1, last)] or [None]
+                    
+        return trees(1, n)
