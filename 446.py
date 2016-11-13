b9ae835f39cd4712d552c64906db0518dcb0c885
co2y <co2y@foxmail.com>
2016-11-13 12:54:00 +0800

    Arithmetic Slices II - Subsequence

---
+++ 446.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def numberOfArithmeticSlices(self, A):
+        """
+        :type A: List[int]
+        :rtype: int
+        """
+        from collections import defaultdict
+        dp = [defaultdict(int) for i in range(len(A))]
+        res = 0
+        for i in range(1, len(A)):
+            for j in range(i):
+                step = A[i] - A[j]
+                dp[i][step] += 1
+                if step in dp[j]:
+                    dp[i][step] += dp[j][step]
+                    res += dp[j][step]
+        return res
