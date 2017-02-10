69fdc9f8191157c37ad09e6287add0ec89b0993a
co2y <co2y@foxmail.com>
2017-02-10 10:28:23 +0800

    Perfect Squares

---
+++ 279.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def numSquares(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        dp = [0xFFFFFFF] * (n + 1)
+        dp[0] = 0
+        dp[1] = 1
+        for i in range(2, n + 1):
+            j = 1
+            while j * j <= i:
+                dp[i] = min(dp[i], dp[i - j * j] + 1)
+                j += 1
+        return dp[n]
