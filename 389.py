ba6c33fe27e27818d30fc9501df1f41c779b04eb
co2y <co2y@foxmail.com>
2017-02-14 14:13:51 +0800

    Find the Difference

---
+++ 389.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def findTheDifference(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: str
+        """
+        from collections import Counter
+
+        for xx in Counter(t) - Counter(s):
+            return xx
