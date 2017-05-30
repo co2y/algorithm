f8cc8f2cee806e23792a200e970c872ab6a9e354
co2y <co2y@foxmail.com>
2017-05-30 18:54:29 +0800

    union find

---
+++ leetcode/547.java
@@ -0,0 +1,13 @@
+public class Solution {
+
+    public int findCircleNum(int[][] M) {
+        int n = M.length;
+        UnionFind uf = new UnionFind(n);
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (M[i][j] == 1) uf.union(i, j);
+            }
+        }
+        return uf.count();
+    }
+}
