a8ed25b7581828726a890ef181919b329baf0e52
co2y <co2y@foxmail.com>
2019-04-21 10:15:58 +0800

    restart day10

--- leetcode/547.java
+++ leetcode/547.java
@@ -1,13 +1,52 @@
-public class Solution {
-
+class Solution {
+    private int count;
+    int[] rank;
+    int[] father;
+    
     public int findCircleNum(int[][] M) {
         int n = M.length;
-        UnionFind uf = new UnionFind(n);
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = i + 1; j < n; j++) {
-                if (M[i][j] == 1) uf.union(i, j);
+        father = new int[n];
+        rank = new int[n];
+        count = n;
+        
+        for (int i = 0; i < n; ++i) {
+            father[i] = i;
+        }
+        
+        for (int i = 0; i < M.length; ++i) {
+            for (int j = i + 1; j < M[i].length; ++j) {
+                if (M[i][j] == 1) {
+                    union(i, j, father);
+                }
             }
         }
-        return uf.count();
+        
+        return count;
+        
+    }
+    
+    private void union(int i, int j, int[] father) {
+        int ir = find(i, father);
+        int jr = find(j, father);
+        if (ir == jr) {
+            return;
+        }
+        
+        if (rank[ir] > rank[jr]) {
+            father[jr] = ir;
+        } else {
+            father[ir] = jr;
+            if (rank[ir] == rank[jr]) {
+                rank[ir]++;
+            }
+        }
+        count--;
+    }
+    
+    private int find(int x, int[] father) {
+        if (father[x] != x) {
+            father[x] = find(father[x], father);
+        }
+        return father[x];
     }
 }
