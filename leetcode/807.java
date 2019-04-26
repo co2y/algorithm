4bbcbb7632a0b8f95e37021566c036a533fa49b2
co2y <co2y@foxmail.com>
2019-04-26 22:39:03 +0800

    practice day15 fk

---
+++ leetcode/807.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int maxIncreaseKeepingSkyline(int[][] grid) {
+        // grid.length = grid[0].length
+        int n = grid.length;
+        int[] col = new int[n];
+        int[] row = new int[n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                row[i] = Math.max(row[i], grid[i][j]);
+                col[j] = Math.max(col[j], grid[i][j]);
+            }
+        }
+        
+        int ans = 0;
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                ans += Math.min(row[i], col[j]) - grid[i][j];
+            }
+        }
+        
+        return ans;
+    }
+}
